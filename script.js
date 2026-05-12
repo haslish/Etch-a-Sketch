b220f96a1bbf3c9a7a8cb0910aee70a9167a1e40
haslish <104933222+haslish@users.noreply.github.com>
2026-05-12 19:40:56 +0200

    Added etch a sketch program

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const divContainer  = document.querySelector("#divContainer")
-const squaresPerRow = 16;
+let squaresPerRow = 16;
+const resizeGrid = document.querySelector("#resizeGrid")
 
 function createGrid (gridWidth){
     for (let i = 0; i<gridWidth; i++){
@@ -18,6 +19,32 @@ function createGrid (gridWidth){
 
 }
 
+
 createGrid (squaresPerRow);
-    
+
+
+divContainer.addEventListener("mouseover", (e) => {
+    const square = e.target.closest(".square");
+    if (!square) return;
+
+    square.style.backgroundColor = "red";
+});
+
+resizeGrid.addEventListener("click", function(){
+    const userInput = prompt("enter new grid size");
+    let newSize = Number(userInput);
+        if (!isNaN(newSize)) {
+
+            if (newSize>100){
+                newSize = 100;
+            }
+            divContainer.innerHTML = "";
+            squaresPerRow = newSize;
+            createGrid (squaresPerRow);
+        }
+});
+
+
+
+
 
